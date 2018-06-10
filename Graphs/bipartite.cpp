e6a501376ebbc940a5a915f0cd8c15dc58d78dec
sonakshisaxena1 <sonakshisaxena1@gmail.com>
2018-06-11 00:48:41 +0530

    new

---
+++ Graphs/bipartite.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+#define V 4
+using namespace std;
+ 
+bool isBipartite(int G[][V], int src)
+{
+    // -1 ==> No color
+    // 1  ==> First color 
+    // 0 ==> Second color 
+    int colorArr[V];
+    for (int i = 0; i < V; ++i)
+        colorArr[i] = -1;
+ 
+
+    colorArr[src] = 1;
+ 
+   
+    queue <int> q;
+    q.push(src);
+ 
+    
+    while (!q.empty())
+    {
+        
+        int u = q.front();
+        q.pop();
+ 
+       
+        if (G[u][u] == 1)
+           return false;  
+ 
+        
+        for (int v = 0; v < V; ++v)
+        {
+           
+            if (G[u][v] && colorArr[v] == -1)
+            {
+                
+                colorArr[v] = 1 - colorArr[u];
+                q.push(v);
+            }
+ 
+            
+            else if (G[u][v] && colorArr[v] == colorArr[u])
+                return false;
+        }
+    }
+ 
+    
+    return true;
+}
+
+int main()
+{
+	freopen("in.in", "r", stdin);
+    freopen("out.out", "w", stdout);
+    int G[][V] = {{0, 1, 0, 1},
+        {1, 0, 1, 0},
+        {0, 1, 0, 1},
+        {1, 0, 1, 0}
+    };
+ 
+    isBipartite(G, 0) ? cout << "Yes" : cout << "No";
+    return 0;
+}
