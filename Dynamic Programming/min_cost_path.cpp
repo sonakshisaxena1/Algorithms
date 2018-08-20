f0c055f866071124e11635e2c84169e4c9331651
sonakshisaxena1 <sonakshisaxena1@gmail.com>
2018-08-20 23:35:38 +0530

    Modified File

--- Dynamic Programming/min_cost_path.cpp
+++ Dynamic Programming/min_cost_path.cpp
@@ -1,33 +1,34 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-#define R 3
-#define C 3
-int minCost(int cost[R][C], int m, int n)
+const int N = 105;
+#define INF 100000005;
+const int R = 3;
+const int C = 3;
+
+
+int minCost(int A[R][C], int m, int n)
 {
-	int dp[m+1][n+1];
-	dp[0][0] = cost[0][0];
-
-	for(int i = 1 ;i <= m; i++)
-		dp[i][0] = dp[i-1][0] + cost[i][0];
-
-	for(int i = 1 ;i <= m; i++)
-		dp[0][i] = dp[0][i-1] + cost[0][i];
-
-	for(int i = 1 ;i <= m; i++)
-		for(int j = 1 ;j <= n; j++)
-			dp[i][j] = min ( min(dp[i-1][j], dp[i][j-1]),
-								     dp[i-1][j-1] ) + cost[i][j];
-
-	return dp[m][n];
+    int dp[R][C];
+    dp[0][0] = A[0][0];
+    for(int i = 1; i < C; i++)
+        dp[0][i] = A[0][i] + dp[0][i-1];
+    for(int i = 1; i < R; i++)
+        dp[i][0] = A[i][0] + dp[i-1][0];
+    for(int i = 1; i < R; i++)
+        for(int j = 1; j < C; j++)
+            dp[i][j] = A[i][j] + min(min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]);
+    return dp[m][n];
 }
-
 int main()
 {
-   freopen("in.in", "r", stdin);
-   freopen("out.out", "w", stdout);
-   int cost[R][C] = { {1, 2, 3},
+    ios::sync_with_stdio(false);
+    freopen("test.in", "r", stdin);
+    freopen("test.out", "w", stdout);
+    
+    int cost[3][3] = { {1, 2, 3},
                       {4, 8, 2},
                       {1, 5, 3} };
-   cout << minCost(cost, 2, 2);
-   return 0;
+   cout << minCost(cost,2,2);
+ 
+    return 0;
 }
